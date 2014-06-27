bbb4dc31ce0346d78c36806dc70340411f65619a
Konstantin Kobs <mail@konstantinkobs.de>
2014-06-27 21:04:37 +0200

    animated loading spinner

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(function(){
     
+    var view = {};
+    
     var template = $("#template").html();
     Mustache.parse(template);
     
     $.getJSON("http://github.wikunia.de/brackets_stats.php?output=json", function(data){
+        view = data;
         var rendered = Mustache.render(template, data);
         $('#content').html(rendered);
     })
